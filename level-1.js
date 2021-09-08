054742eebdecfba4c1c920c19cf42fd47f36f4f9
Andrew <andrewvun96@yandex.ru>
2021-09-09 01:37:00 +0300

    Решил первую задачу из домашнего задания

--- level-1.js
+++ level-1.js
@@ -1,6 +1,8 @@
 /* Создать переменную numberOfFilms и в нее поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?/ */
 
-let numberOfFilms = 'Сколько фильмов вы уже посмотрели?';
+"use strict";
 
-alert(numberOfFilms, "");
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
+
+alert(numberOfFilms);
